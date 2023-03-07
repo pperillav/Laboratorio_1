7db40dd97198b9cded29bb3d2885d1a61ce8eaf0
Pedro Perilla <pperillav@unal.edu.co>
2023-03-06 23:37:25 -0500

    v3_6_3_2023

--- programa.js
+++ programa.js
@@ -17,3 +17,16 @@ let resultado = calcular(5,4);
 //imprimir el área
 console.log(resultado[0])
 console.log(resultado[1])
+
+function calcular_hipotenusa(cat_a, cat_b){
+
+    const hip;
+    hip=Math.sqrt(cat_a**2+cat_b**2);
+
+    return hip
+}
+
+let resultado_2 = calcular_hipotenusa(2,4);
+
+//imprimir el área
+console.log(resultado_2)
